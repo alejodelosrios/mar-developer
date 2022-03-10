849bcf4c2eef3990a45816d5bf10bdbd20164e7b
Alejandro Ramirez <ramalejandro@gmail.com>
2022-03-10 11:51:15 -0600

    test(HomePage): Simple tests updated 🚨

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App component', () => {
+	beforeEach(() => {
+		render(
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		)
+	})
+
+	it('renders component title', () => {
+		expect(screen.getByText(/home/i)).toBeInTheDocument();
+	});
+})
