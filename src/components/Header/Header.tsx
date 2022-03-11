cffcfec46e9bfe8caa07ba02f9aa221ff8eed4dd
Alejandro Ramirez <ramalejandro@gmail.com>
2022-03-11 10:59:25 -0600

    style(Home Page): Format & some css styles added 💄

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import ME from '../../assets/perfil_manuel.png'
+import { Cta } from '../Cta/Cta'
 
 export const Header = () => {
-	return (
-		<div className="font-poppins">
-			<h1>Header</h1>
-		</div>
-	)
+  return (
+    <header className="-poppins">
+      <h5>Hello, I'm</h5>
+      <h1 className="text-center" data-testid="h1-title">
+        Manuel Alejandro
+        <br />
+        <span> Ramírez</span>
+      </h1>
+      <h5>FullStack Developer</h5>
+      <Cta />
+      <div>
+        <img src={ME} alt="me" data-testid="profile-img" />
+      </div>
+      <a href="#contact">Scroll Down</a>
+    </header>
+  )
 }
