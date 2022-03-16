0e141e97fed97d11732c1c2368bb817da0330f0d
Alejandro Ramirez <ramalejandro@gmail.com>
2022-03-16 17:45:39 -0600

    feat(Portfolio): ViwMore functionality added ✨
    - New viwMore local state added to the component
    - Test updated to include the viewMore functionality
    - Some CSS styles applied

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -1,12 +1,14 @@
 import { SectionTitle } from '../SectionTitle/SectionTitle'
 import { ProjectCard } from './ProjectCard/ProjectCard'
 import { projects } from '../../utils/projects'
+import { useState } from 'react'
 
 export const Portfolio = () => {
+  const [viewMore, setViewMore] = useState<boolean>(false)
   return (
-    <section className="mx-4 mt-32 md:mx-0">
+    <section className="mx-4 mt-32 md:mx-0 flex flex-col items-center">
       <SectionTitle title="Portfolio" subtitle="My Recent Work" />
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6">
+      <div className="w-max grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 place-content-center">
         {projects &&
           projects.map((project) => (
             <ProjectCard
@@ -18,6 +20,28 @@ export const Portfolio = () => {
             ></ProjectCard>
           ))}
       </div>
+      {!viewMore && (
+        <button onClick={() => setViewMore(true)} className="btn my-6">
+          View more ...
+        </button>
+      )}
+      {viewMore && (
+        <div
+          data-testid="viewMore-container"
+          className="mt-4 w-max grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 place-content-center"
+        >
+          {projects &&
+            projects.map((project) => (
+              <ProjectCard
+                key={project.id}
+                title={project.title}
+                image={project.image}
+                github={project.github}
+                web={project.web}
+              ></ProjectCard>
+            ))}
+        </div>
+      )}
     </section>
   )
 }
--- src/components/Portfolio/ProjectCard/ProjectCard.tsx
+++ src/components/Portfolio/ProjectCard/ProjectCard.tsx
@@ -8,14 +8,21 @@ type Props = {
 
 export const ProjectCard = ({ github, web, image, title, children }: Props) => {
   return (
-    <article className="relative">
-      <img className="" src={image} alt={`img ${title}`} />
-      <small>Tags</small>
-      <h3>{title}</h3>
+    <article
+      id="project-card"
+      className="relative w-[360px] h-[260px] flex flex-col justify-center items-center cursor-pointer bg-secondary dark:bg-transparent dark:border dark:border-lightgray  hover:border-transparent rounded-t-xl transition-all duration-1000"
+    >
+      <img
+        className="absolute left-0 top-0 w-full h-full opacity-0 hover:opacity-100 transition duration-[2000ms] rounded-t-xl"
+        src={image}
+        alt={`img ${title}`}
+      />
+      <small className="transition opacity-100  hover:opacity-0 duration-[1500ms] ">Tags</small>
+      <h3 className="transition opacity-100  hover:opacity-0 duration-1000 text-2xl">{title}</h3>
       {children}
       {github && (
         <a
-          className="absolute bottom-[48px] left-0 bg-primary text-cgray px-2 py-1 text-sm cursor-pointer"
+          className="absolute bottom-0 left-0 bg-primary text-cgray px-2 py-1 text-sm cursor-pointer"
           href={github}
         >
           GitHub
@@ -23,7 +30,7 @@ export const ProjectCard = ({ github, web, image, title, children }: Props) => {
       )}
       {web && (
         <a
-          className="absolute bottom-[48px] right-0 bg-primary text-cgray px-2 py-1 text-sm cursor-pointer"
+          className="absolute bottom-0 right-0 bg-primary text-cgray px-2 py-1 text-sm cursor-pointer"
           href={web}
         >
           Live
